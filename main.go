47c175ca792fb45ff4e43866e2ff3c37b4fc86a7
xuwei <xuwei@aixin-ins.com>
2019-09-29 09:07:44 +0800

    change gin

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	_ "hello/routers"
-	"github.com/astaxie/beego"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func main() {
-	beego.Run()
+func main(){
+	r := gin.Default()
+	r.LoadHTMLGlob("views/*")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
-
